a456b3ef70b6646043b570877c3d289a908577e6
DONADEI Emrick <emrick.donadei@hotmail.fr>
2019-06-17 23:24:32 +0200

    finishing css, adding first components for testing, and beginning the setup of react router

--- src/App.js
+++ src/App.js
@@ -2,21 +2,23 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from '@material-ui/icons/Home';
+import Navar from './components/Navbar'
+import Navbar from './components/Navbar';
+import Cart from './components/Cart';
+import Product from './components/Product';
+import ProductList from './components/ProductList';
+import Default from './components/Default';
+import Details from './components/Details';
 
 function App() {
   return (
-    <div>
-      <div className="container">
-        <div className="row">
-          <div className="col-6">column one</div>
-          <div className="col-6">
-          <Home/>
-          </div>
-          
-        </div>
-      </div>
-
-    </div>
+    <React.Fragment>
+      <Navbar/>
+      <ProductList></ProductList>
+      <Details></Details>
+      <Cart></Cart>
+      <Default></Default>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Hello from Navbar</h3>
+            </div>
+        );
+    }
+}
+
+export default Navbar;
