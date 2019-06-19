1d8cc5a03fe4b46dd68b831ceb01150b7c0eab42
DONADEI <emrick.donadei@3ds.com>
2019-06-19 13:55:40 +0200

    React router basic logic and starting to work on the navbar

--- src/App.js
+++ src/App.js
@@ -2,22 +2,24 @@ import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from '@material-ui/icons/Home';
-import Navar from './components/Navbar'
 import Navbar from './components/Navbar';
 import Cart from './components/Cart';
 import Product from './components/Product';
 import ProductList from './components/ProductList';
 import Default from './components/Default';
 import Details from './components/Details';
+import {Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
     <React.Fragment>
       <Navbar/>
-      <ProductList></ProductList>
-      <Details></Details>
-      <Cart></Cart>
-      <Default></Default>
+      <Switch>
+        <Route exact path="/" component={ProductList}></Route>
+        <Route path="/details" component={Details}></Route>
+        <Route path="/cart" component={Cart}></Route>
+        <Route component={Default}></Route>
+      </Switch>
     </React.Fragment>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+import logo from "../logo.svg";
 
 class Navbar extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Hello from Navbar</h3>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+        <Link to="/">
+          <img src={logo} alt="store" className="navbar-brand" />
+          
+        </Link>
+      </nav>
+    );
+  }
 }
 
 export default Navbar;
