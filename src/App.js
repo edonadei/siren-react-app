9e6da5240ae0215726144a3f0a38bee0e09842fb
DONADEI <emrick.donadei@3ds.com>
2019-06-21 16:02:47 +0200

    add styled packages and finishing navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from '@material-ui/icons/Home';
 import Navbar from './components/Navbar';
 import Cart from './components/Cart';
-import Product from './components/Product';
 import ProductList from './components/ProductList';
 import Default from './components/Default';
 import Details from './components/Details';
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,34 +1,45 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import AccessAlarmIcon from "@material-ui/icons/AccessAlarm";
+import Cart from "@material-ui/icons/ShoppingCart";
+import Button from "@material-ui/core/Button";
 import logo from "../logo.svg";
+import styled from "styled-components"
 
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
+      <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
-          
         </Link>
         <ul className="navbar-nav align-items-center">
           <li className="nav-item ml-5">
-
             <Link to="/" className="nav-link">
               Products
             </Link>
           </li>
         </ul>
 
-        <Link to='/cart' className="ml-auto">
-        <button>
-          <i className="fas fa-cart-plus" />
-          My cart
-        </button>
+        <Link to="/cart" className="ml-auto">
+          <Button variant="outlined" color="primary">
+            <span className="mr-2">
+              <Cart />
+            </span>
+            My cart
+          </Button>
         </Link>
-      </nav>
+      </NavWrapper>
     );
   }
 }
+
+const NavWrapper = styled.nav`
+background: var(--mainBlue);
+.nav-link {
+  color: var(--mainWhite) !important;
+  font-size: 1.3rem;
+  text-transform:capitalize !important;
+}
+`;
 
 export default Navbar;
