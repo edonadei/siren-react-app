de50893f156765df02218a129f1ef09f6e9921d9
DONADEI <emrick.donadei@3ds.com>
2019-06-21 13:57:41 +0200

    fill navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,10 +6,25 @@ import logo from "../logo.svg";
 class Navbar extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark px-sm-5">
+      <nav className="navbar navbar-expand-sm bg-primary navbar-dark px-sm-5">
         <Link to="/">
           <img src={logo} alt="store" className="navbar-brand" />
           
+        </Link>
+        <ul className="navbar-nav align-items-center">
+          <li className="nav-item ml-5">
+
+            <Link to="/" className="nav-link">
+              Products
+            </Link>
+          </li>
+        </ul>
+
+        <Link to='/cart' className="ml-auto">
+        <button>
+          <i className="fas fa-cart-plus" />
+          My cart
+        </button>
         </Link>
       </nav>
     );
