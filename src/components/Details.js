fb834486c5a33f1a0252af039077c736bdf16520
DONADEI Emrick <emrick.donadei@hotmail.fr>
2019-07-02 11:45:11 +0200

    beginning work on detail page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react';
+import { ProductConsumer } from '../context'
+import { Link } from 'react-router-dom'
+import { ButtonContainer } from './Button'
 
 class Details extends Component {
     render() {
         return (
-            <div>
-             <h3>Hello from details</h3>   
-            </div>
+            <ProductConsumer>
+                {value => {
+                    const { id, company, img, info, price, title, inCard } =
+                        value.detailProducts;
+                    return (
+                        <div className="container py-5">
+                            {/* title */}
+                            <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                                <h1>{title}</h1>
+                            </div>
+                            {/* end title */}
+
+                            {/* product info */}
+                            
+                            {/* end product info */}
+                            
+                        </div>
+                    )
+                }}
+            </ProductConsumer>
         );
     }
 }
